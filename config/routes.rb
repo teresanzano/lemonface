dd355a6d9ac8ff4996a4e7a8618ca453609dadfb
Tesa <Tmaranzano@gmail.com>
2014-04-30 17:09:41 -0400

    Added Pins to the header bar -- by myself. wut

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Pinteresting::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
   get "faq" => "pages#faq"
+  get "pins" => "pins#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
