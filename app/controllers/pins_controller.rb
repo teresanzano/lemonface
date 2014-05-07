7bbda45b57a9aeb3fbfaad151357c557ae7f0391
Tesa <Tmaranzano@gmail.com>
2014-05-06 20:22:35 -0400

    added pagination

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -6,8 +6,8 @@ class PinsController < ApplicationController
 
 
   def index
-    @pins = Pin.all
-  end
+   @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
+ end
 
   def show
   end
